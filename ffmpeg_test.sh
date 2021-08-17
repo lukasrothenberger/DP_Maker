f6ab58b897ff812947e918671896462fd39106a8
Lukas Rothenberger <lukas_rothenberger@hotmail.de>
2021-08-17 19:48:03 +0200

    .

--- ffmpeg_test.sh
+++ ffmpeg_test.sh
@@ -2,7 +2,7 @@
 #make clean
 #cd ..
 
-python -m Makefile_Analyzer --t=/home/lukas/Schreibtisch/ffmpeg/Makefile --dp-path=/home/lukas/git/discopop/ --dp-build-path=/home/lukas/git/discopop/build/ --exec-mode=cu_gen
+python -m Makefile_Analyzer --target-project=/home/lukas/Schreibtisch/ffmpeg --target-makefile=/home/lukas/Schreibtisch/ffmpeg/Makefile --dp-path=/home/lukas/git/discopop/ --dp-build-path=/home/lukas/git/discopop/build/ --exec-mode=cu_gen
 #cd atax_Makefile && ./out && cd ..
 
 #python -m Makefile_Analyzer --t=atax_Makefile/Makefile --dp-path=/home/lukas/git/discopop/ --dp-bui=/home/lukas/git/discopop/build/ --exec-mode=cu_gen
