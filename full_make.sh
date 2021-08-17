381137dd285ebee03f46094a92326097f2cbe0d5
Lukas Rothenberger <lukas_rothenberger@hotmail.de>
2021-08-17 11:03:16 +0200

    added script for convenience

---
+++ full_make.sh
@@ -0,0 +1,13 @@
+cd atax_Makefile
+make clean
+cd ..
+
+python -m Makefile_Analyzer --t=atax_Makefile/Makefile --dp-path=/home/lukas/git/discopop/ --dp-bui=/home/lukas/git/discopop/build/ --exec-mode=dep
+cd atax_Makefile && ./out && cd ..
+
+python -m Makefile_Analyzer --t=atax_Makefile/Makefile --dp-path=/home/lukas/git/discopop/ --dp-bui=/home/lukas/git/discopop/build/ --exec-mode=cu_gen
+cd atax_Makefile && ./out && cd ..
+
+python -m Makefile_Analyzer --t=atax_Makefile/Makefile --dp-path=/home/lukas/git/discopop/ --dp-bui=/home/lukas/git/discopop/build/ --exec-mode=dp_red
+cd atax_Makefile && ./out && cd ..
+
