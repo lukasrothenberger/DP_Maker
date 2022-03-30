a6cbcf2010fc0a9d7aab0497c50ca6534caab16d
goerlibe <goerlibe@users.noreply.github.com>
2022-03-30 09:08:57 +0200

    examples: move common variables to separate file

--- examples/atax/make_atax.sh
+++ examples/atax/make_atax.sh
@@ -1,6 +1,8 @@
-HOME_DIR=/home/goerlibe/discopop/DP_Maker/
 TARGET_PROJECT_FOLDER=/home/goerlibe/discopop/DP_Maker/examples/atax/atax_Makefile
 TARGET_FOLDER=$TARGET_PROJECT_FOLDER
+
+# load configuration variables that are similar for all example builds (e.g. DP_PATH)
+source ../config.sh
 
 # needed if you want to rebuild the entire application
 cd $TARGET_FOLDER
@@ -12,19 +14,19 @@ cd $HOME_DIR
 #  2) run the generated tmp_makefile.mk
 
 # obtain computational units (CUs)
-python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --dp-path=/home/goerlibe/discopop/discopop --dp-build-path=/home/goerlibe/discopop/discopop/build --exec-mode=dep
+python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --dp-path=$DP_PATH --dp-build-path=$DP_BUILD --exec-mode=dep
 cd $TARGET_FOLDER && make -f tmp_makefile.mk && ./out && cd $HOME_DIR
 
 # identify data dependencies by instrumentation
-python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --dp-path=/home/goerlibe/discopop/discopop --dp-build-path=/home/goerlibe/discopop/discopop/build --exec-mode=cu_gen
+python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --dp-path=$DP_PATH --dp-build-path=$DP_BUILD --exec-mode=cu_gen
 cd $TARGET_FOLDER && make -f tmp_makefile.mk && ./out && cd $HOME_DIR
 
 # find inter-iteration data dependencies that can be resolved using OpenMP reduction clause
-python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --dp-path=/home/goerlibe/discopop/discopop --dp-build-path=/home/goerlibe/discopop/discopop/build --exec-mode=dp_red
+python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --dp-path=$DP_PATH --dp-build-path=$DP_BUILD --exec-mode=dp_red
 cd $TARGET_FOLDER && make -j7 -f tmp_makefile.mk && ./out && cd $HOME_DIR
 
 
-# run discopop to obtain parallelization opportunities
+# run discopop to obtain parallelization opportunities "pop.txt"
 cd $TARGET_FOLDER && python -m discopop_explorer --cu-xml=Data.xml --dep-file=out_dep.txt >> pop.txt
 
 cd $HOME_DIR
--- examples/lulesh/make-lulesh-makefile.sh
+++ examples/lulesh/make-lulesh-makefile.sh
@@ -1,6 +1,8 @@
-HOME_DIR=/home/goerlibe/discopop/DP_Maker/
 TARGET_PROJECT_FOLDER=/home/goerlibe/discopop/DP_Maker/examples/lulesh/LULESH
 TARGET_FOLDER=$TARGET_PROJECT_FOLDER
+
+# load configuration variables that are similar for all example builds (e.g. DP_PATH)
+source ../config.sh
 
 # force a complete rebuild by cleaning beforehand
 cd $TARGET_PROJECT_FOLDER
@@ -11,18 +13,18 @@ make clean
 #  2) run the generated tmp_makefile.mk
 
 # obtain computational units (CUs)
-cd $HOME_DIR && python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --compiler=mpic++ --dp-path=/home/goerlibe/discopop/discopop --dp-build-path=/home/goerlibe/discopop/discopop/build --exec-mode=dep
+cd $HOME_DIR && python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --compiler=mpic++ --dp-path=${DP_PATH} --dp-build-path=${DP_BUILD} --exec-mode=dep
 cd $TARGET_FOLDER && make -f tmp_makefile.mk && ./lulesh2.0  -s 12
 
 # identify data dependencies by instrumentation
-cd $HOME_DIR && python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --compiler=mpic++ --dp-path=/home/goerlibe/discopop/discopop --dp-build-path=/home/goerlibe/discopop/discopop/build --exec-mode=cu_gen
+cd $HOME_DIR && python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --compiler=mpic++ --dp-path=${DP_PATH} --dp-build-path=${DP_BUILD} --exec-mode=cu_gen
 cd $TARGET_FOLDER && make -f tmp_makefile.mk && ./lulesh2.0  -s 12
 
 # find inter-iteration data dependencies that can be resolved using OpenMP reduction clause
-cd $HOME_DIR && python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --compiler=mpic++ --dp-path=/home/goerlibe/discopop/discopop --dp-build-path=/home/goerlibe/discopop/discopop/build --exec-mode=dp_red
+cd $HOME_DIR && python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --compiler=mpic++ --dp-path=${DP_PATH} --dp-build-path=${DP_BUILD} --exec-mode=dp_red
 cd $TARGET_FOLDER && make -j7 -f tmp_makefile.mk && ./lulesh2.0 -s 12
 
 
-# run discopop to obtain parallelization opportunities
+# run discopop to obtain parallelization opportunities "pop.txt"
 cd $TARGET_FOLDER && python -m discopop_explorer --cu-xml=Data.xml --dep-file=lulesh2.0_dep.txt >> pop.txt
 cd $HOME_DIR
