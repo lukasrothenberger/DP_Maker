d5017a9dfc54c61d091e66a0a28d94826761e14b
goerlibe <goerlibe@users.noreply.github.com>
2022-05-18 10:25:12 +0200

    cleanup atax example

--- examples/atax/make_atax.sh
+++ examples/atax/make_atax.sh
@@ -1,32 +1,40 @@
-TARGET_PROJECT_FOLDER=/home/goerlibe/discopop/DP_Maker/examples/atax/atax_Makefile
-TARGET_FOLDER=$TARGET_PROJECT_FOLDER
+TARGET_FOLDER=/home/goerlibe/discopop/DP_Maker/examples/atax/atax_Makefile
 
 # load configuration variables that are similar for all example builds (e.g. DP_PATH)
 source ../config.sh
 
-# needed if you want to rebuild the entire application
+# preparation
 cd $TARGET_FOLDER
 make clean
+mkdir discopop
 cd $HOME_DIR
 
-# general approach when using dp-maker:
-#  1) start the Makefile_Analyzer with the desired exec-mode (cu_gen, dep, dp_red)
-#  2) run the generated tmp_makefile.mk
-
 # obtain computational units (CUs)
-python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --dp-path=$DP_PATH --dp-build-path=$DP_BUILD --exec-mode=dep
-cd $TARGET_FOLDER && make -f tmp_makefile.mk && ./out && cd $HOME_DIR
+python -m Makefile_Analyzer --target-project=$TARGET_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --dp-path=$DP_PATH --dp-build-path=$DP_BUILD --exec-mode=cu_gen
+cd $TARGET_FOLDER && make -f tmp_makefile.mk && mv tmp_makefile.mk discopop/tmp_makefile_cu.mk && cd $HOME_DIR
+
 
 # identify data dependencies by instrumentation
-python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --dp-path=$DP_PATH --dp-build-path=$DP_BUILD --exec-mode=cu_gen
-cd $TARGET_FOLDER && make -f tmp_makefile.mk && ./out && cd $HOME_DIR
+python -m Makefile_Analyzer --target-project=$TARGET_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --dp-path=$DP_PATH --dp-build-path=$DP_BUILD --exec-mode=dep
+cd $TARGET_FOLDER && make -f tmp_makefile.mk && ./out && mv tmp_makefile.mk discopop/tmp_makefile_dep.mk && cd $HOME_DIR
 
 # find inter-iteration data dependencies that can be resolved using OpenMP reduction clause
-python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --dp-path=$DP_PATH --dp-build-path=$DP_BUILD --exec-mode=dp_red
-cd $TARGET_FOLDER && make -j7 -f tmp_makefile.mk && ./out && cd $HOME_DIR
+python -m Makefile_Analyzer --target-project=$TARGET_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --dp-path=$DP_PATH --dp-build-path=$DP_BUILD --exec-mode=dp_red
+cd $TARGET_FOLDER && make -j7 -f tmp_makefile.mk && mv tmp_makefile.mk discopop/tmp_makefile_red.mk && cd $HOME_DIR
 
+# cleanup
+cd $TARGET_FOLDER
+rm *.ll
+rm out
+mv Data.xml discopop/Data.xml
+mv out_dep.txt discopop/dp_run_dep.txt
+mv FileMapping.txt discopop/FileMapping.txt
+mv OriginalVariables.txt discopop/OriginalVariables.txt
+mv DP_CUIDCounter.txt discopop/DP_CUIDCounter.txt
+mv loop_counter_output.txt discopop/loop_counter_output.txt
+mv reduction.txt discopop/reduction.txt
 
-# run discopop to obtain parallelization opportunities "pop.txt"
-cd $TARGET_FOLDER && python -m discopop_explorer --cu-xml=Data.xml --dep-file=out_dep.txt >> pop.txt
+# run discopop to obtain parallelization opportunities ranked_patterns.txt
+python -m discopop_explorer --path=discopop >> discopop/ranked_patterns.txt
 
 cd $HOME_DIR
--- examples/config.sh
+++ examples/config.sh
@@ -1,7 +1,6 @@
 # this file is `source`ed by other scripts inside the examples directory to import common configuration variables
 # make sure to edit these values to your needs
 
-HOME_DIR=/home/goerlibe/discopop/DP_Maker/
-DP_PATH=/home/goerlibe/discopop/discopop/
-DP_BUILD=${DP_PATH}/build/
-#llvm / clang / ...?
+HOME_DIR=/home/goerlibe/discopop/DP_Maker
+DP_PATH=/home/goerlibe/discopop/discopop
+DP_BUILD=${DP_PATH}/build
