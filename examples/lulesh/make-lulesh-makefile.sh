3edefe931f6fa31b7cfc397bcae6f3767801d08c
goerlibe <goerlibe@users.noreply.github.com>
2022-05-23 12:49:33 +0200

    cleanup of lulesh example

--- examples/lulesh/make-lulesh-makefile.sh
+++ examples/lulesh/make-lulesh-makefile.sh
@@ -1,30 +1,49 @@
-TARGET_PROJECT_FOLDER=/home/goerlibe/discopop/DP_Maker/examples/lulesh/LULESH
-TARGET_FOLDER=$TARGET_PROJECT_FOLDER
+# colorful output :)
+OUT_HIGHLIGHT='\033[1;33m'
+OUT_STANDARD='\033[0m'
 
 # load configuration variables that are similar for all example builds (e.g. DP_PATH)
 source ../config.sh
 
-# force a complete rebuild by cleaning beforehand
-cd $TARGET_PROJECT_FOLDER
+# project specific variables
+TARGET_PROJECT=/home/goerlibe/discopop/DP_Maker/examples/lulesh/LULESH
+EXEC_ARGS="-s 5 -i 72"
+
+# cleanup to force complete rebuild
+cd $TARGET_PROJECT
 make clean
+rm -rf $TARGET_PROJECT/discopop
+mkdir $TARGET_PROJECT/discopop
 
-# general approach when using dp-maker:
-#  1) start the Makefile_Analyzer with the desired exec-mode (cu_gen, dep, dp_red)
-#  2) run the generated tmp_makefile.mk
+echo -e "${OUT_HIGHLIGHT} --> CU GENERATION <--${OUT_STANDARD}"
+cd $HOME_DIR && python -m Makefile_Analyzer --target-project=$TARGET_PROJECT --target-makefile=$TARGET_PROJECT/Makefile --compiler=g++ --dp-path=${DP_PATH} --dp-build-path=${DP_BUILD} --exec-mode=cu_gen
+cd $TARGET_PROJECT && make -f tmp_makefile.mk
+mv tmp_makefile.mk $TARGET_PROJECT/discopop/tmp_makefile_cu.mk
 
-# obtain computational units (CUs)
-cd $HOME_DIR && python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --compiler=mpic++ --dp-path=${DP_PATH} --dp-build-path=${DP_BUILD} --exec-mode=dep
-cd $TARGET_FOLDER && make -f tmp_makefile.mk && ./lulesh2.0  -s 12
+echo -e "${OUT_HIGHLIGHT} --> INSTRUMENTATION <--${OUT_STANDARD}"
+cd $HOME_DIR && python -m Makefile_Analyzer --target-project=$TARGET_PROJECT --target-makefile=$TARGET_PROJECT/Makefile --compiler=g++ --dp-path=${DP_PATH} --dp-build-path=${DP_BUILD} --exec-mode=dep
+cd $TARGET_PROJECT && make -f tmp_makefile.mk && ./lulesh2.0  $EXEC_ARGS
+mv tmp_makefile.mk $TARGET_PROJECT/discopop/tmp_makefile_inst.mk
 
-# identify data dependencies by instrumentation
-cd $HOME_DIR && python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --compiler=mpic++ --dp-path=${DP_PATH} --dp-build-path=${DP_BUILD} --exec-mode=cu_gen
-cd $TARGET_FOLDER && make -f tmp_makefile.mk && ./lulesh2.0  -s 12
+echo -e "${OUT_HIGHLIGHT} --> REDUCTION <--${OUT_STANDARD}"
+cd $HOME_DIR && python -m Makefile_Analyzer --target-project=$TARGET_PROJECT --target-makefile=$TARGET_PROJECT/Makefile --compiler=g++ --dp-path=${DP_PATH} --dp-build-path=${DP_BUILD} --exec-mode=dp_red
+cd $TARGET_PROJECT && make -j7 -f tmp_makefile.mk
+mv tmp_makefile.mk $TARGET_PROJECT/discopop/tmp_makefile_red.mk
 
-# find inter-iteration data dependencies that can be resolved using OpenMP reduction clause
-cd $HOME_DIR && python -m Makefile_Analyzer --target-project=$TARGET_PROJECT_FOLDER --target-makefile=$TARGET_FOLDER/Makefile --compiler=mpic++ --dp-path=${DP_PATH} --dp-build-path=${DP_BUILD} --exec-mode=dp_red
-cd $TARGET_FOLDER && make -j7 -f tmp_makefile.mk && ./lulesh2.0 -s 12
+# cleanup
+mv Data.xml $TARGET_PROJECT/discopop/Data.xml
+mv lulesh2.0_dep.txt $TARGET_PROJECT/discopop/dp_run_dep.txt
+mv FileMapping.txt $TARGET_PROJECT/discopop/FileMapping.txt
+mv OriginalVariables.txt $TARGET_PROJECT/discopop/OriginalVariables.txt
+mv DP_CUIDCounter.txt $TARGET_PROJECT/discopop/DP_CUIDCounter.txt
+mv loop_counter_output.txt $TARGET_PROJECT/discopop/loop_counter_output.txt
+mv reduction.txt $TARGET_PROJECT/discopop/reduction.txt
+rm -f *.ll
+rm -f lulesh2.0
+rm -f lulesh2.0_dp_inst.ll
 
 
-# run discopop to obtain parallelization opportunities "pop.txt"
-cd $TARGET_FOLDER && python -m discopop_explorer --cu-xml=Data.xml --dep-file=lulesh2.0_dep.txt >> pop.txt
+# run discopop to obtain parallelization opportunities
+echo -e "${OUT_HIGHLIGHT} --> RUNNING DISCOPOP EXPLORER <--${OUT_STANDARD}"
+python -m discopop_explorer --path=$TARGET_PROJECT/discopop >> $TARGET_PROJECT/discopop/ranked_patterns.txt
 cd $HOME_DIR
