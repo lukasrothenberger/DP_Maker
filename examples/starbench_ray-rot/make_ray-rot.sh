8b76a4d9a50a83f90b476c403d6a4df4f915d46b
goerlibe <goerlibe@users.noreply.github.com>
2022-05-27 09:37:11 +0200

    add example starbench ray-rot

---
+++ examples/starbench_ray-rot/make_ray-rot.sh
@@ -0,0 +1,12 @@
+source ../config.sh
+source ../functions.sh
+
+TARGET_PROJECT="$DP_MAKER_BASE_PATH/examples/starbench_ray-rot/ray-rot"
+TARGET_MAKEFILE="$TARGET_PROJECT/Makefile"
+COMPILER="g++"
+RESULT_DIR="$TARGET_PROJECT/discopop"
+RESULT_FILENAME="result.txt"
+EXECUTABLE="ray-rot"
+EXECUTION_COMMAND="$TARGET_PROJECT/ray-rot scene $DP_MAKER_BASE_PATH/examples/starbench_ray-rot/ray-rot/discopop/result.ppm 45 320 180 5"
+
+execute_DP_Maker
---
+++ examples/starbench_ray-rot/ray-rot/discopop/tmp_makefile_cu.mk
@@ -0,0 +1,12 @@
+all: -1 4
+-1:
+0:
+	cd /home/goerlibe/discopop/DP_Maker/examples/starbench_ray-rot/ray-rot && clang image.cpp -c -o image.ll -g -O0 -S -emit-llvm -fno-discard-value-names -Xclang -load -Xclang /home/goerlibe/discopop/discopop/build/libi/LLVMCUGeneration.so -mllvm -fm-path -mllvm /home/goerlibe/discopop/DP_Maker/examples/starbench_ray-rot/ray-rot/FileMapping.txt;
+1:
+	cd /home/goerlibe/discopop/DP_Maker/examples/starbench_ray-rot/ray-rot && clang rotation_engine.cpp -c -o rotation_engine.ll -g -O0 -S -emit-llvm -fno-discard-value-names -Xclang -load -Xclang /home/goerlibe/discopop/discopop/build/libi/LLVMCUGeneration.so -mllvm -fm-path -mllvm /home/goerlibe/discopop/DP_Maker/examples/starbench_ray-rot/ray-rot/FileMapping.txt;
+2:
+	cd /home/goerlibe/discopop/DP_Maker/examples/starbench_ray-rot/ray-rot && clang ray_engine.cpp -c -o ray_engine.ll -g -O0 -S -emit-llvm -fno-discard-value-names -Xclang -load -Xclang /home/goerlibe/discopop/discopop/build/libi/LLVMCUGeneration.so -mllvm -fm-path -mllvm /home/goerlibe/discopop/DP_Maker/examples/starbench_ray-rot/ray-rot/FileMapping.txt;
+3:
+	cd /home/goerlibe/discopop/DP_Maker/examples/starbench_ray-rot/ray-rot && clang program.cpp -c -o program.ll -g -O0 -S -emit-llvm -fno-discard-value-names -Xclang -load -Xclang /home/goerlibe/discopop/discopop/build/libi/LLVMCUGeneration.so -mllvm -fm-path -mllvm /home/goerlibe/discopop/DP_Maker/examples/starbench_ray-rot/ray-rot/FileMapping.txt;
+4: 0 1 2 3
+	cd /home/goerlibe/discopop/DP_Maker/examples/starbench_ray-rot/ray-rot && clang++ image.ll rotation_engine.ll ray_engine.ll program.ll -o ray-rot -L/home/goerlibe/discopop/discopop/build/rtlib -lDiscoPoP_RT -lpthread;
